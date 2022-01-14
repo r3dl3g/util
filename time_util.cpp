4f3c748f7601ed81c416b846876f1bcb52691819
Armin Rothfuss <armin@rothfuss-web.de>
2022-01-14 11:21:15 +0100

    Fix file_time_pount to time_point conversion.

--- time_util.cpp
+++ time_util.cpp
@@ -234,15 +234,13 @@ namespace util {
       const auto systemTime = std::chrono::clock_cast<std::chrono::system_clock>(ftp);
       const time_point tp = std::chrono::system_clock::to_time_t(systemTime);
 #elif __cplusplus < 201401L
-      // this is gcc 5.1 or greater
+      // this is gcc 5.1 or pior
       std::time_t tt = file_time_point::clock::to_time_t(ftp);
       const auto tp = std::chrono::system_clock::from_time_t(tt);
 #else
       using namespace std::chrono;
-      auto sctp = time_point_cast<system_clock::duration>(ftp - file_time_point::clock::now()
+      const time_point tp = time_point_cast<system_clock::duration>(ftp - file_time_point::clock::now()
                   + system_clock::now());
-      const auto tse = system_clock::to_time_t(sctp);
-      const time_point tp = time_point(time_point::duration(tse));
 #endif
       return format_datetime(tp, date_delem, separator, time_delem, add_millis);
     }
